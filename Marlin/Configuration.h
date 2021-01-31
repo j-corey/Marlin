184b91d201732cd64c2fb6b03f548f24da50a396
j-corey <edmondsjc+git@gmail.com>
2021-01-31 12:56:33 -0700

    MANUAL_Y_HOME_POS to get nozzle home onto bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1381,8 +1381,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS 0
-//#define MANUAL_Y_HOME_POS 0
+#define MANUAL_X_HOME_POS 0           // move nozzle right (neg dir) 0 mm
+#define MANUAL_Y_HOME_POS -5          // move plate forward (neg dir) 5 mm
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
