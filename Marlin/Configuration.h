0562936314072fdb09c27c2db916017bd8aa3fb5
j-corey <edmondsjc+git@gmail.com>
2021-02-26 14:16:16 -0700

    LCD timeout, M73 enable, G26 enable

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1138,8 +1138,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS -10            // move nozzle home 10mm back, onto the bed, past clips
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE+X_MIN_POS
-#define Y_MAX_POS Y_BED_SIZE+Y_MIN_POS
+#define X_MAX_POS X_BED_SIZE-X_MIN_POS    // corrected formula
+#define Y_MAX_POS Y_BED_SIZE-Y_MIN_POS    // corrected formula
 #define Z_MAX_POS 250
 
 /**
@@ -1275,7 +1275,7 @@
   /**
    * Enable the G26 Mesh Validation Pattern tool.
    */
-  //#define G26_MESH_VALIDATION
+  #define G26_MESH_VALIDATION         // was disabled
   #if ENABLED(G26_MESH_VALIDATION)
     #define MESH_TEST_NOZZLE_SIZE    0.4  // (mm) Diameter of primary nozzle.
     #define MESH_TEST_LAYER_HEIGHT   0.2  // (mm) Default layer height for the G26 Mesh Validation Tool.
