c1cabebeeeabd96007195d895bfdb3841272befd
j-corey <edmondsjc+git@gmail.com>
2021-05-20 07:02:59 -0700

    Z_MIN_POS to 0.2 (how was this changed?, MAX = 220

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1137,10 +1137,10 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS -10            // move nozzle home 10mm back, onto the bed, past clips
-#define Z_MIN_POS 0
+#define Z_MIN_POS 0.2        // was 0; endstop switch stops nozzle z at this position
 #define X_MAX_POS X_BED_SIZE-X_MIN_POS    // corrected formula
 #define Y_MAX_POS Y_BED_SIZE-Y_MIN_POS    // corrected formula
-#define Z_MAX_POS 250
+#define Z_MAX_POS 220       // was 250; limited by control cable length
 
 /**
  * Software Endstops
